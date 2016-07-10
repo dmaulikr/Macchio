4ec18396bc5afcf89a02c370672ec1193e0261ee
rkanderson <ryananderson@Ryans-MacBook-Pro-3.local>
2016-07-10 15:08:16 -0700

    Initial commit

---
+++ DotIOGame/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  DotIOGame
+//
+//  Created by Ryan Anderson on 7/10/16.
+//  Copyright (c) 2016 Ryan Anderson. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        
+    }
+}
