91f2e5b04cd116dc7dad34ace4249a4c263f695a
rkanderson <ryananderson@Ryans-MacBook-Pro-3.local>
2016-07-11 13:50:07 -0700

    A crude-looking joystick appears in addition to the arrow. Both the arrow and the joystick can be toggled on and off through and options tuple.

--- DotIOGame/GameScene.swift
+++ DotIOGame/GameScene.swift
@@ -10,6 +10,10 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var prefs = (
+        showJoyStick: false,
+        showArrow: true
+    )
     var previousTime: CFTimeInterval? = nil
     var player: PlayerCreature!
     var directionArrow: SKSpriteNode!
@@ -18,6 +22,8 @@ class GameScene: SKScene {
     let playerSpeed: CGFloat = 100
     var playerMovingTouch: UITouch? = nil
     var originalMovingTouchPositionInCamera: CGPoint? = nil
+    var joyStickBox: SKNode!, controlStick: SKNode!
+    let maxControlStickDistance: CGFloat = 20
     
     override func didMoveToView(view: SKView) {
         player = PlayerCreature(name: "Yoloz Boy 123")
@@ -31,21 +37,29 @@ class GameScene: SKScene {
         directionArrow.zRotation = player.velocity.angle
         directionArrow.hidden = true
         camera!.addChild(directionArrow)
+        
+        joyStickBox = childNodeWithName("//joyStickBox")
+        controlStick = childNodeWithName("//controlStick")
+        joyStickBox.hidden = true
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touch in touches {
             if playerMovingTouch == nil {
                 playerMovingTouch = touch
-                
                 let location = touch.locationInNode(camera!)
                 originalMovingTouchPositionInCamera = location
                 
+                if prefs.showArrow {
+                    directionArrow.hidden = false
+                    directionArrow.position = convertPoint(convertPoint(CGPoint(x: player.size.width + minDirectionArrowDistanceFromPlayer, y: 0), fromNode: player), toNode: camera!)
+                    directionArrow.zRotation = player.velocity.angle - CGFloat(90).degreesToRadians()
+                }
                 
-                directionArrow.hidden = false
-                
-                directionArrow.position = convertPoint(convertPoint(CGPoint(x: player.size.width + minDirectionArrowDistanceFromPlayer, y: 0), fromNode: player), toNode: camera!)
-                directionArrow.zRotation = player.velocity.angle - CGFloat(90).degreesToRadians()
+                if prefs.showJoyStick {
+                    joyStickBox.position = originalMovingTouchPositionInCamera!
+                    joyStickBox.hidden = false
+                }
             }
         }
     }
@@ -58,13 +72,24 @@ class GameScene: SKScene {
                 
                 player.velocity.angle = angle
                 
-                // My new means of determining the position of the arrow:
-                // the arrow will be straight ahead of the player's eyeball. How far it is is the distance the current touch location is from its orignal position. I will add a clamp too
-                var pointInRelationToPlayer = CGPoint(x: player.size.width + location.distanceTo(originalMovingTouchPositionInCamera!), y: 0)
-                pointInRelationToPlayer.x.clamp(player.size.width + minDirectionArrowDistanceFromPlayer, player.size.width + maxDirectionArrowDistanceFromPlayer)
-                directionArrow.position = convertPoint(convertPoint(pointInRelationToPlayer, fromNode: player), toNode: camera!)
+                if prefs.showArrow {
+                    // My means of determining the position of the arrow:
+                    // the arrow will be straight ahead of the player's eyeball. How far it is is the distance the current touch location is from its orignal position. I have a value clamp too.
+                    var pointInRelationToPlayer = CGPoint(x: player.size.width + location.distanceTo(originalMovingTouchPositionInCamera!), y: 0)
+                    pointInRelationToPlayer.x.clamp(player.size.width + minDirectionArrowDistanceFromPlayer, player.size.width + maxDirectionArrowDistanceFromPlayer)
+                    directionArrow.position = convertPoint(convertPoint(pointInRelationToPlayer, fromNode: player), toNode: camera!)
+                    directionArrow.zRotation = player.velocity.angle - CGFloat(90).degreesToRadians()
+                }
                 
-                directionArrow.zRotation = player.velocity.angle - CGFloat(90).degreesToRadians()
+                if prefs.showJoyStick {
+                    //Move controlStick based on finger movement. Also add a distance cap
+                    controlStick.position = location - originalMovingTouchPositionInCamera!
+                    if location.distanceTo(originalMovingTouchPositionInCamera!) > maxControlStickDistance {
+                        let angle = atan2(controlStick.position.y, controlStick.position.x)
+                        controlStick.position.x = cos(angle) * maxControlStickDistance
+                        controlStick.position.y = sin(angle) * maxControlStickDistance
+                    }
+                }
             }
         }
     }
@@ -74,7 +99,13 @@ class GameScene: SKScene {
             if touch == playerMovingTouch {
                 playerMovingTouch = nil
                 originalMovingTouchPositionInCamera = nil
-                directionArrow.hidden = true
+                if prefs.showArrow {
+                    directionArrow.hidden = true
+                }
+                if prefs.showJoyStick {
+                    joyStickBox.hidden = true
+                    controlStick.position = CGPoint(x: 0, y: 0)
+                }
             }
         }
     }
